13f781d8e7185235e3473fe752e60bdaea35f9ee
Takeyuki UEDA <ueda@latelierdueda.com>
2020-12-30 19:27:21 +0900

    sticky footer

--- src/App.svelte
+++ src/App.svelte
@@ -16,16 +16,42 @@
 </script>
 
 <main>
-	<AppBar bind:active={active}/>
+	<div class="appbar">
+		<AppBar bind:active={active}/>
+	</div>
 
-	<div style="position:relative;">
+	<div style="position:relative;" class="body">
+
 		<Navigation bind:active={active}/>
 
 		<Router {routes}/>
 
+	</div>
+	
+	<div class="footer">
 		<Footer/>
 	</div>
+
 </main>
 
 <style>
+main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+}
+
+.appbar {
+    flex-shrink: 0;
+}
+.body{
+    flex-grow: 1;
+    overflow: auto;
+    min-height: 2em;
+}
+.footer{
+    flex-shrink: 0;
+}
 </style>
